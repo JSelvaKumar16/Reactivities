332120a2602aad8f38a31f75bc50d98f8ee35efa
Selva Kumar J <j.selvakumar@gmail.com>
2025-02-25 11:47:57 +0530

    End of Section 16

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -13,14 +13,7 @@ namespace Application.Activities
         {
             public Activity Activity { get; set; }
         }
-
-        /*  public class CommandValidator : AbstractValidator<Command>
-         {
-             public CommandValidator()
-             {
-                 RuleFor(x => x.Activity).SetValidator(new ActivityValidator());
-             }
-         } */
+      
         public class Handler : IRequestHandler<Command, Result<Unit>>
         {
             private readonly DataContext _context;
